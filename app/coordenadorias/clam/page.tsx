e1a0e09063e969fe3e14f3f7cfaa1c33ee7f1f37
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2025-08-12 12:33:09 -0300

    resolvendo build

--- app/coordenadorias/clam/page.tsx
+++ app/coordenadorias/clam/page.tsx
@@ -3,51 +3,12 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import './style.css';
-
-const ligasBasico = [
-    'Liga Acadêmica de Comunicação, Humanização e Plantão da Palhaçada (LACOHP)',
-    'Liga Acadêmica de Fisiologia Médica (LAFIM)',
-    'Liga Acadêmica de Genética Médica (LAGEM)',
-    'Liga Acadêmica de Medicina do Esporte (LAME)',
-    'Liga Acadêmica de Nutrologia (LANUT)',
-    'Liga Acadêmica de Patologia de Araguari (LAPA)',
-    'Liga Acadêmica de Primeiros Socorros (LAPS)',
-    'Liga Acadêmica de Semiologia Médica (LASEM)',
-    'Liga Acadêmica de Farmacologia (LAFARM)',
-    'Liga Acadêmica de Medicina da Família e Comunidade (LAMFAC)',
-    'Liga Acadêmica de Gestão e Empreendedorismo (LAGET)',
-    'Liga Acadêmica de Cuidados Paliativos (LACP)'
-];
-
-const ligasClinico = [
-    'Liga Acadêmica de Cardiologia (LACOR)',
-    'Liga Acadêmica de Dermatologia (LADERM)',
-    'Liga Acadêmica de Ginecologia e Obstetrícia (LAGO)',
-    'Liga Acadêmica de Neurologia (LANE)',
-    'Liga Acadêmica de Oncologia (LAONCO)',
-    'Liga Acadêmica de Reumatologia (LAR)',
-    'Liga Acadêmica de Psiquiatria (LUPA)',
-    'Liga Acadêmica de Anestesiologia, Reanimação e Dor (LAARD)',
-    'Liga Acadêmica de Oftalmologia (LAOA)',
-    'Liga Universitária de Mastologia (LUMA)',
-    'Liga Acadêmica de Pediatria (LAPED)',
-    'Liga Universitária de Otorrinolaringologia (LAOTO)',
-    'Liga Universitária de Medicina de Emergência (LUME)',
-    'Liga Acadêmica de Medicina Intensiva (LIGAMI)',
-    'Liga Acadêmica de Pneumologia (LIAP)',
-    'Liga Acadêmica de Clínica Cirúrgica (LACCA)',
-    'Liga Acadêmica de Endocrinologia (LAE)',
-    'Liga Acadêmica de Nefrologia e Urologia (LANEU)',
-    'Liga Acadêmica de Cirurgia Plástica (LAPLAST)',
-    'Liga Acadêmica de Medicina Legal e Perícia Médica (LAMELP)',
-    'Liga Acadêmica de Ortopedia e Traumatologia (LAOT)',
-    'Liga Acadêmica de Clínica Médica e Saúde (LUCMSC)',
-    'Liga Acadêmica de Infectologia e Imunologia Clínica (LAIIC)',
-    'Liga Acadêmica de Gastroenterologia (LAGASTRO)'
-];
+import { IAcademicLeague } from '@/app/lib/models/AcademicLeagues';
+import Link from 'next/link';
 
 export default function CLAMPage() {
-    const [activeSection, setActiveSection] = useState<string | null>(null);
+    const [activeSection, setActiveSection] = useState<"basic" | "clinic" | null>(null);
+    const [data, setData] = useState<Pick<IAcademicLeague, "_id" | "name" | "acronym" | "type">[]>([]);
 
     useEffect(() => {
         // Criar partículas
@@ -64,7 +25,21 @@ export default function CLAMPage() {
         }
     }, []);
 
-    const toggleSection = (section: string) => {
+    useEffect(() => {
+        // Criar partículas
+        const fetchData = async () => {
+            const data = await fetch('/api/get/getAllNamesAndAcronym');
+            if (!data.ok) {
+                alert('Network response was not ok')
+                throw new Error('Network response was not ok');
+            }
+            const json = await data.json();
+            setData(json.data);
+        }
+        fetchData();
+    }, []);
+
+    const toggleSection = (section: "basic" | "clinic") => {
         setActiveSection(activeSection === section ? null : section);
     };
 
@@ -72,7 +47,6 @@ export default function CLAMPage() {
         <div className="clam-container">
             <div className="clam-background"></div>
             <div className="particles"></div>
-            
             <div className="clam-content">
                 {/* Seção do Logo e Título */}
                 <div className="logo-container">
@@ -85,7 +59,7 @@ export default function CLAMPage() {
                             className="logo-image"
                         />
                     </div>
-                    
+
                     <div className="title-container">
                         <h1 className="title-glow">CLAM</h1>
                         <h2 className="text-3xl text-white/90 font-medium tracking-wide">
@@ -109,7 +83,48 @@ export default function CLAMPage() {
                     </p>
                 </div>
 
-                {/* Seções Colapsáveis */}
+                <div className="collapsible">
+                    <div
+                        className="collapsible-header"
+                        onClick={() => toggleSection('basic')}
+                    >
+                        <span>Ciclo Básico</span>
+                        <span className={`arrow ${activeSection === 'basic' ? 'active' : ''}`}>▼</span>
+                    </div>
+                    <div className={`collapsible-content ${activeSection === 'basic' ? 'active' : ''}`}>
+                        <ul className="liga-list">
+                            {data.filter((value) => value.type === "basic").map((league, index) => (
+                                <li key={index} className="liga-item">
+                                    <Link href={`/coordenadorias/clam/liga/${league._id}`}>
+                                        {league.name} - ({league.acronym})
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="collapsible">
+                    <div
+                        className="collapsible-header"
+                        onClick={() => toggleSection('clinic')}
+                    >
+                        <span>Ciclo Clínico</span>
+                        <span className={`arrow ${activeSection === 'clinic' ? 'active' : ''}`}>▼</span>
+                    </div>
+                    <div className={`collapsible-content ${activeSection === 'clinic' ? 'active' : ''}`}>
+                        <ul className="liga-list">
+                            {data.filter((value) => value.type === "clinic").map((league, index) => (
+                                <li key={index} className="liga-item">
+                                    <Link href={`/coordenadorias/clam/liga/${league._id}`}>
+                                        {league.name} - ({league.acronym})
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                {/*
+
                 <div className="collapsible">
                     <div 
                         className="collapsible-header"
@@ -143,6 +158,8 @@ export default function CLAMPage() {
                         </ul>
                     </div>
                 </div>
+                */}
+
             </div>
         </div>
     );
--- app/lib/models/index.ts
+++ app/lib/models/index.ts
@@ -1,5 +1,6 @@
 import "./CertificateModel"
 import "./EventCertificateModel"
+import "./AcademicLeagues"
 // Continue importando os demais modelos...
 
 console.log("Todos os modelos foram registrados corretamente!");
