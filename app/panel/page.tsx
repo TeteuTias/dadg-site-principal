31ba0083eae2598fe546c4b503d8eb7931b7bf57
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2026-03-09 22:16:45 -0300

    apenas para teste

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -5,7 +5,7 @@ export default function () {
     const a = useUserContext()
     return (
         <main className="w-full min-h-screen bg-yellow-100">
-            <h1 onClick={() => console.log(a)}>Estou Autenticado - { }</h1>
+            <h1 onClick={() => console.log(a)}>Estou Autenticado - {a.tokenVar}</h1>
         </main>
     )
 }
